b9053bcc6af40061b4022e558b7ecba650075466
Nathanzera <134970379+NathanTorrente@users.noreply.github.com>
2024-10-22 17:09:52 -0400

    Alteração 1

---
+++ script.js
@@ -0,0 +1,76 @@
+//console.log("Hello Wold");
+
+var nome = 'Ney';
+var sobrenome = 'torrente';
+var idade = 18; /*variavel numerica*/ 
+
+const i = 18;
+var aprovada = false;
+var NomeCompleto = nome +' '+sobrenome;
+
+
+var numero1 = 10;
+var numero2 = 5;
+var soma = numero1 + numero2; /*Soma*/ 
+var sub = numero1 - numero2;  /*Subtração */
+var mult = numero1 * numero2; /* Multiplicação */
+var div = numero1 / numero2;    /*Divisão*/
+var mod = 13 % 6;              /*Modulo*/ 
+var exp = 3 ** 2;               /*Expoente*/
+
+
+console.log('A soma é: ' + soma);
+console.log('A subtração é: ' + sub);
+console.log('A multiplicação é: ' + mult);
+console.log('A divisão é: ' + div);
+console.log('O modulo é: ' + mod);
+console.log('O expoente é: ' + exp);
+
+/*if(exp < 10){
+    console.log('menor que 10')
+}
+else{
+    console.log('maior que 10')
+}
+
+
+*/
+
+/*
+var a = 10;
+var b = '10';
+if(a === b || a < 10){
+    console.log('São iguais')
+}
+else{
+    console.log('Não são iguais')
+}
+*/
+
+if(blur.isNan){
+    console.log('Não é número');
+}
+else{
+    console.log('é número!');
+}
+
+/*
+
+ + Soma
+ - Subtração
+ * Multiplicação
+ / Divisão
+ % Modulo = resto de uma divisão
+ ** expoente
+
+ */
+
+
+/*
+console.log(NomeCompleto);
+console.log(typeof idade);
+console.log(mod);
+console.log(exp);
+
+console.log(NomeCompleto, idade);
+*/
---
+++ script2.js
@@ -0,0 +1,48 @@
+var idade = 18;
+
+
+if (idade < 18){
+    console.log('Você é menor de idade');
+}
+
+else if (idade >= 18 && idade <= 60){
+    console.log('Rapaz, o homi ta adutu');
+}
+else if (idade > 60 && idade < 70){
+    console.log('terceira idade, não aposentado é claro, continue trabaiando');
+}
+else if( idade >= 70){
+console.log('terceira idade, aposentado')
+}
+
+function areaQuadrado(lado){
+    area = lado * lado;
+    return area;
+
+}
+
+
+
+var r = areaQuadrado(10);
+console.log(r);
+
+
+
+function CalculoIMC(altura, peso){
+    imc = peso / (altura * altura);
+    return imc;
+}
+var imc = CalculoIMC(1.69, 55)
+console.log('O valor do imc é:' + imc);
+
+
+for(var i = 0; i < 10; i++){
+
+    console.log(i);
+}
+
+var n = 0;
+while(n>10){
+    console.log(n);
+    n++;
+}
